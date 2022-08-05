e7cde453d00544c5b5cceddc6e0285f2c30d8aa4
vysaghkrishnan <vysaghkrishnan4u@gmail.com>
2022-08-05 13:07:27 +0100

    the changes have commited

--- vpc.tf
+++ vpc.tf
@@ -12,10 +12,6 @@ provider "google" {
 }
 
 # VPC
-resource "google_compute_network" "vpc" {
-  name                    = "${var.project_id}-vpc"
-  auto_create_subnetworks = "false"
-}
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
