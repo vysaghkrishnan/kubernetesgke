2cefce3f54722b75a8d3b2579dc67ca314c97893
vysaghkrishnan <vysaghkrishnan4u@gmail.com>
2022-08-05 13:08:27 +0100

    the changes have commited

--- vpc.tf
+++ vpc.tf
@@ -17,6 +17,6 @@ provider "google" {
 resource "google_compute_subnetwork" "subnet" {
   name          = "${var.project_id}-subnet"
   region        = var.region
-  network       = google_compute_network.vpc.name
+ 
   ip_cidr_range = "10.10.0.0/24"
 }                                        
